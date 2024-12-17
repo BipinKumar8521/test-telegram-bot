fa2f028bd492ad3de8eecf836a1ec33486967580
Bipin <bk3284488@gmail.com>
2024-12-17 15:28:30 +0530

    some changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require("telegraf");
+const express = require("express");
 
 require("dotenv").config();
 const token = process.env.BOT_TOKEN;
@@ -9,25 +10,25 @@ const bot = new Telegraf(token);
 bot.start((ctx) => ctx.reply("Welcome to the bot!"));
 
 bot.command("help", (ctx) => {
-  ctx.reply("Type anything to get started");
+	ctx.reply("Type anything to get started");
 });
 
 bot.command("google", (ctx) => {
-  generateLink(link, ctx);
+	generateLink(link, ctx);
 });
 
 bot.command("gmail", (ctx) => {
-  generateLink("https://mail.google.com/mail/u/0/#inbox", ctx);
+	generateLink("https://mail.google.com/mail/u/0/#inbox", ctx);
 });
 
 //get the user input
 bot.on("text", (ctx) => {
-  const userInput = ctx.message.text;
-  if (userInput.startsWith("https://")) {
-    generateLink(userInput, ctx);
-  } else {
-    ctx.reply("Please send a link to open");
-  }
+	const userInput = ctx.message.text;
+	if (userInput.startsWith("https://")) {
+		generateLink(userInput, ctx);
+	} else {
+		ctx.reply("Please send a link to open");
+	}
 });
 
 bot.launch();
@@ -35,9 +36,21 @@ bot.launch();
 console.log("Bot is running");
 
 function generateLink(link, ctx) {
-  ctx.reply("Open the link with button below", {
-    reply_markup: {
-      keyboard: [[{ text: "Open Link", web_app: { url: link } }]],
-    },
-  });
+	ctx.reply("Open the link with button below", {
+		reply_markup: {
+			keyboard: [[{ text: "Open Link", web_app: { url: link } }]],
+		},
+	});
 }
+
+const app = express();
+app.use(bot.webhookCallback("/secret-path"));
+
+app.get("/", (req, res) => {
+	bot.launch();
+	res.send("Bot is running");
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
